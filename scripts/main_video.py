5a27ba2a449ce86313c49595e6a7eb3b203d8f6a
manasd28 <manasdalakoti@gmail.com>
2020-12-10 16:02:11 +0530

    Added the weights

--- scripts/main_video.py
+++ scripts/main_video.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2
 import time
+import keras
 
 
 # Reading the input from the file.
@@ -16,6 +17,52 @@ h, w = None, None
 # Loading the file that contains the classes names information.
 with open('../data_files/classes.names') as f:
     labels = [line.strip() for line in f]
+    
+# Loading the pre-trained model
+model = keras.models.load_model('../data_files/mnist_cnn2.h5')
+
+# Label of the trained model
+model_labels = ['Speed limit (20km/h)',
+ 'Speed limit (30km/h)',
+ 'Speed limit (50km/h)',
+ 'Speed limit (60km/h)',
+ 'Speed limit (70km/h)',
+ 'Speed limit (80km/h)',
+ 'End of speed limit (80km/h)',
+ 'Speed limit (100km/h)',
+ 'Speed limit (120km/h)', 'No passing',
+ 'No passing for vehicles over 3.5 metric tons',
+ 'Right-of-way at the next intersection',
+ 'Priority road',
+ 'Yield',
+ 'Stop',
+ 'No vehicles',
+ 'Vehicles over 3.5 metric tons prohibited',
+ 'No entry',
+ 'General caution',
+ 'Dangerous curve to the left',
+ 'Dangerous curve to the right',
+ 'Double curve',
+ 'Bumpy road',
+ 'Slippery road',
+ 'Road narrows on the right',
+ 'Road work',
+ 'Traffic signals',
+ 'Pedestrians',
+ 'Children crossing',
+ 'Bicycles crossing', 'Beware of ice/snow',
+ 'Wild animals crossing',
+ 'End of all speed and passing limits',
+ 'Turn right ahead',
+ 'Turn left ahead',
+ 'Ahead only',
+ 'Go straight or right',
+ 'Go straight or left',
+ 'Keep right',
+ 'Keep left',
+ 'Roundabout mandatory',
+ 'End of no passing',
+ 'End of no passing by vehicles over 3.5 metric tons']
 
 # Loading the trained configuration file and trained weights.
 network = cv2.dnn.readNetFromDarknet('../data_files/ts.cfg',
